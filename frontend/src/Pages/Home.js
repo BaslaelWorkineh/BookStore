c5f17ac85bc7d219afb460d6fa29bbc7a0879f43
Baslael Workineh <baslaworku@gmail.com>
2024-04-11 20:28:53 +0300

    deleted unused

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import BannerArea from '../components/BannerArea'
 import '../components/css/main.css'
-import '../components/css/animate.min.css'
 import CallToAction from '../components/CallToAction'
 import BooksList from '../components/Books'
-import Footer from '../components/Footer'
+import FooterClass from '../components/Footer'
 
 export const Home = () => {
   return (
     <div>
       <BannerArea />
       <BooksList />
-      <Footer />
+      <FooterClass />
     </div>
   )
 }
